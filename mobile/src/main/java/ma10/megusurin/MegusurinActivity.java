3083ef1db88143afc9d9cc8737b17a06c44378a3
egg <t.egawa@gmail.com>
2014-09-13 12:51:50 +0900

    Enabled to receive fire and thunder events for mobile

--- mobile/src/main/java/ma10/megusurin/MegusurinActivity.java
+++ mobile/src/main/java/ma10/megusurin/MegusurinActivity.java
@@ -2,15 +2,41 @@ package ma10.megusurin;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MegusurinActivity extends Activity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.wearable.DataApi;
+import com.google.android.gms.wearable.DataEventBuffer;
+import com.google.android.gms.wearable.MessageApi;
+import com.google.android.gms.wearable.MessageEvent;
+import com.google.android.gms.wearable.Node;
+import com.google.android.gms.wearable.NodeApi;
+import com.google.android.gms.wearable.Wearable;
+
+public class MegusurinActivity extends Activity implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener, DataApi.DataListener, MessageApi.MessageListener,
+        NodeApi.NodeListener {
+
+
+    private static final String TAG = "Megusurin";
+    private static final String PATH_FIRE = "/fire";
+    private static final String PATH_THUNDER = "/thunder";
+
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_megusurin);
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Wearable.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
     }
 
 
@@ -31,5 +57,69 @@ public class MegusurinActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Wearable.DataApi.removeListener(mGoogleApiClient, this);
+        Wearable.MessageApi.removeListener(mGoogleApiClient, this);
+        Wearable.NodeApi.removeListener(mGoogleApiClient, this);
+        mGoogleApiClient.disconnect();
+    }
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        Log.d(TAG, "onConnected(): Successfully connected to Google API client");
+        Wearable.DataApi.addListener(mGoogleApiClient, this);
+        Wearable.MessageApi.addListener(mGoogleApiClient, this);
+        Wearable.NodeApi.addListener(mGoogleApiClient, this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int cause) {
+        Log.d(TAG, "onConnectionSuspended(): Connection to Google API client was suspended");
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        Log.e(TAG, "onConnectionFailed(): Failed to connect, with result: " + result);
+    }
+
+    @Override
+    public void onMessageReceived(final MessageEvent event) {
+        Log.d(TAG, "onMessageReceived: " + event);
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                String path = event.getPath();
+                if (PATH_FIRE.equals(path)) {
+                    Toast.makeText(getApplicationContext(), "Fire event received.", Toast.LENGTH_SHORT).show();
+                } else if (PATH_THUNDER.equals(path)) {
+                    Toast.makeText(getApplicationContext(), "Thunder event received", Toast.LENGTH_SHORT).show();
+                } else {
+                    Log.d(TAG, "Unknown path: " + path);
+                }
+            }
+        });
+    }
+    @Override
+    public void onDataChanged(DataEventBuffer dataEvents) {
+        Log.d(TAG, "onDataChanged: " + dataEvents);
+    }
+    @Override
+    public void onPeerConnected(Node node) {
+        Log.d(TAG, "Node Connected:" + node.getId());
+    }
+
+    @Override
+    public void onPeerDisconnected(Node node) {
+        Log.d(TAG, "Node Disconnected:" + node.getId());
     }
 }
